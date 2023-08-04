22c6783fd550ec8a09ca71e7c85273d77a282a2d
FrhCode <farhan7534031b@gmail.com>
2023-08-04 19:28:53 +0700

    progrees

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,15 @@ export default function Home() {
       <Jumbotron />
       <div className="h-10"></div>
       <div className="px-[5vw] py-11">
-        <div className="mx-auto flex max-w-7xl items-center justify-between rounded-md bg-gray-100 px-10 py-16">
-          HI
+        <div className="mx-auto max-w-7xl rounded-md bg-gray-100 p-16">
+          <div className="grid w-full grid-cols-12">
+            <p className="col-span-5 text-4xl leading-snug">
+              Having a hard time keeping up with JavaScript?
+            </p>
+            <p className="col-span-5 col-start-7 text-4xl leading-snug text-gray-400">
+              Having a hard time keeping up with JavaScript?
+            </p>
+          </div>
         </div>
       </div>
     </>
