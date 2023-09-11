dba34b4ccbb338d531f871690cae017446c74dd6
FrhCode <farhan7534031b@gmail.com>
2023-09-11 12:02:53 +0700

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,11 @@ export default function Home() {
         <div className="mx-auto max-w-7xl rounded-md bg-gray-100 p-16">
           <div className="grid w-full grid-cols-12 gap-8 lg:gap-4">
             <p className="col-span-6 text-4xl leading-snug lg:col-span-12">
-              Kesulitan dalam mengenali penyakit yang menyerang ikan lele anda?
+              Kesulitan mengenali penyakit pada ikan lele anda?
             </p>
             <p className="col-span-6 col-start-7 text-4xl leading-snug text-gray-400 lg:col-span-12">
-              Kebetulan, Anda berada ditempat yang tepat. Dokter lele merupakan
-              satu-satunya platform yang anda butuhkan untuk menyelesaikan
-              masalah anda.
+              Anda berada ditempat yang tepat. Dokter lele merupakan platform
+              untuk menyelesaikan masalah anda.
             </p>
           </div>
         </div>
