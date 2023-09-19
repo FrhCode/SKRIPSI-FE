723944bad40ec3230fd295b2ff8b7be9aa256a4e
FrhCode <farhan7534031b@gmail.com>
2023-09-19 15:37:00 +0700

    progress

--- service/consultation/getOneConsultation.ts
+++ service/consultation/getOneConsultation.ts
@@ -2,7 +2,7 @@ import { fetcher } from "../fetcher";
 import InvoiceData from "@/types/Consultation";
 
 export async function getOneConsultation(
-  invoiceNumber: string = "INV-20230918-1"
+  invoiceNumber: string = "INV-20230919-1"
 ): Promise<InvoiceData> {
   return fetcher<InvoiceData>(`/api/v1/consultations/${invoiceNumber}`);
 }
